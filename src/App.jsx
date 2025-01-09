cfb621c8bbe39d95e2eb3708acaf8c0eb1ee2064
Ezekiel Mokaya Ogana <ezekielmokayaogana>
2025-01-09 13:15:31 +0300

    first innit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header/Header.jsx"
+import Footer from "./Footer/Footer.jsx";
+import Body from "./Body/Body.jsx"; 
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App
